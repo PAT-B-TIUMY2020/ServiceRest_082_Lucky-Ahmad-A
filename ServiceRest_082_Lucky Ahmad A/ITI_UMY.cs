2a392ff342e98d11c84052933a66661c0ff8d6a0
UB8082 <lucky.ahmad.ft18@mail.umy.ac.id>
2020-12-21 20:08:00 +0700

    Add project files.

---
+++ ServiceRest_082_Lucky Ahmad A/ITI_UMY.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Services;
+using System.Data.SqlClient;
+
+namespace ServiceRest_082_Lucky_Ahmad_A
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITI_UMY
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa", ResponseFormat = WebMessageFormat.Json)] // Untuk membuat slash, selalu relative
+        List<Mahasiswa> GetAllMahasiswa(); // Mendapatkan kumpulan mahasiswa / seluruh data mahasiswa
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa/nim={nim}", ResponseFormat = WebMessageFormat.Json)] // untuk get
+        Mahasiswa GetMahasiswaByNIM(string nim); //mengambil data berdasarkan nim
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Mahasiswa", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string CreateMahasiswa(Mahasiswa mhs);
+
+    }
+
+    [DataContract]
+    public class Mahasiswa
+    {
+        //Adalah konversi atau kesepakatan //variable lokal
+        private string _nama, _nim, _prodi, _angkatan;
+        [DataMember(Order = 1)] //mengirim data untuk mengurutkan
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+        [DataMember(Order = 2)]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+        [DataMember(Order = 3)]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+        [DataMember(Order = 4)]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+    }
+}
---
+++ ServiceRest_082_Lucky Ahmad A/TI_UMY.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ServiceRest_082_Lucky_Ahmad_A
+{
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class TI_UMY : ITI_UMY
+    {
+        SqlConnection sqlConnection;
+        string connectionString = "Data Source=DESKTOP-OLFSSOV;Initial catalog=TI UMY;Persist Security Info=True;User ID=sa;Password=123";
+
+        public string CreateMahasiswa(Mahasiswa mhs)
+        {
+            string msg = "Gagal";
+
+            sqlConnection = new SqlConnection(connectionString);
+            string query = string.Format("Insert into dbo.Mahasiswa values ('{0}', '{1}', '{2}', '{3}')", mhs.nim, mhs.nama, mhs.prodi, mhs.angkatan);
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+
+            try
+            {
+                sqlConnection.Open();
+                Console.WriteLine(query);
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                msg = "Sukses";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return msg;
+        }
+
+        public List<Mahasiswa> GetAllMahasiswa()
+        {
+            List<Mahasiswa> mahas = new List<Mahasiswa>();
+            sqlConnection = new SqlConnection(connectionString);
+            string query = "Select NIM, Nama, Prodi, Angkatan from dbo.Mahasiswa";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+
+            try
+            {
+                sqlConnection.Open();
+                //mendapatkan data yang telah diexecute
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Mahasiswa mhs = new Mahasiswa();
+                    mhs.nim = reader.GetString(0);
+                    mhs.nama = reader.GetString(1);
+                    mhs.prodi = reader.GetString(2);
+                    mhs.angkatan = reader.GetString(3);
+
+                    mahas.Add(mhs);
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return mahas;
+        }
+
+        public Mahasiswa GetMahasiswaByNIM(string nim)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            sqlConnection = new SqlConnection(connectionString);
+            string query = String.Format("Select NIM, Nama, Prodi, Angkatan from dbo.Mahasiswa where NIM = '{0}'", nim);
+            SqlCommand com = new SqlCommand(query, sqlConnection);
+
+            try
+            {
+                sqlConnection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    mhs.nim = reader.GetString(0);
+                    mhs.nama = reader.GetString(1);
+                    mhs.prodi = reader.GetString(2);
+                    mhs.angkatan = reader.GetString(3);
+                }
+                com.Clone();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return mhs;
+        }
+
+    }
+}
